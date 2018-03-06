f2cc431883a8bfda1588893e0fbe779598d085be
Nicolas Carlier <n.carlier@nunux.org>
2018-03-06 22:14:08 +0100

    chore(): remove gofeed dependency with the model

--- pkg/model/article.go
+++ pkg/model/article.go
@@ -1,7 +1,15 @@
 package model
 
-import "github.com/mmcdole/gofeed"
+import "time"
 
 type Article struct {
-	gofeed.Item
+	Title           string     `json:"title,omitempty"`
+	Description     string     `json:"description,omitempty"`
+	Content         string     `json:"content,omitempty"`
+	Link            string     `json:"link,omitempty"`
+	Updated         string     `json:"updated,omitempty"`
+	UpdatedParsed   *time.Time `json:"updatedParsed,omitempty"`
+	Published       string     `json:"published,omitempty"`
+	PublishedParsed *time.Time `json:"publishedParsed,omitempty"`
+	GUID            string     `json:"guid,omitempty"`
 }
