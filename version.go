65217fc2b2fc694c75a2aa455ceefce5255f3b8a
Nicolas Carlier <n.carlier@nunux.org>
2019-04-25 09:27:13 +0000

    feat(): export configuration variables

--- version.go
+++ version.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"fmt"
 )
 
@@ -21,4 +22,9 @@ This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 
 Written by Nicolas Carlier.`, version)
+}
+
+func init() {
+	expvar.NewString("version").Set(Version)
+	expvar.NewString("rev").Set(GitCommit)
 }
