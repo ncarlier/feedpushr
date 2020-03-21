4e39bf08c15bb2fed9a04e7c6e7d8ce1f54c4bd5
Nicolas Carlier <n.carlier@nunux.org>
2020-03-21 18:32:49 +0000

    fix(format): improve tweet function security

--- pkg/format/fn/tweet.go
+++ pkg/format/fn/tweet.go
@@ -4,6 +4,12 @@ import "fmt"
 
 // Tweet truncate text if needed and put the link after
 func Tweet(text string, suffix string) string {
-	text = Truncate(270-len(suffix), text)
+	toTruncate := 270 - len(suffix)
+	if toTruncate > 0 {
+		text = Truncate(toTruncate, text)
+	} else {
+		// suffix too long, text is sacrificed
+		return suffix
+	}
 	return fmt.Sprintf("%s\n%s", text, suffix)
 }
