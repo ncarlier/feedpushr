a914213313fd35b218436c5e10f88db023f78001
Christian Winther <jippignu@gmail.com>
2022-12-20 09:14:31 +0100

    feat(contrib): add language selector for mastodon plugin
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- contrib/mastodon/client.go
+++ contrib/mastodon/client.go
@@ -12,6 +12,9 @@ type Toot struct {
 	Status     string `json:"status"`
 	Sensitive  bool   `json:"sensitive"`
 	Visibility string `json:"visibility"`
+	// if language is not configured in the UI, the field will be an empty string, and the 'omitempty' ensures that the field won't be
+	// sent upstream as an empty string, ensuring full backwards compatibility for people upgrading and not reconfiguring their output
+	Language string `json:"language,omitempty"`
 }
 
 func sendToMastodon(toot Toot, url string, accessToken string) error {
